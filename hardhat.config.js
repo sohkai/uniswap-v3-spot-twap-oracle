e1cd0af4aad325131e48e6404a4addb56a770752
Brett Sun <qisheng.brett.sun@gmail.com>
2021-08-20 16:04:47 +0200

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-etherscan')
+require('hardhat-local-networks-config-plugin')
+
+module.exports = {
+  solidity: {
+    version: '0.7.6',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 999999,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+}
