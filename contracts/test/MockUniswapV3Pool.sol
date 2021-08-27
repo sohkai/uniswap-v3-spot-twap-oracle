a24dd81cca312ef765d57d540f7ae0154ce0a522
Brett Sun <qisheng.brett.sun@gmail.com>
2021-08-27 12:53:02 +0200

    test: add MockUniswapV3Pool

---
+++ contracts/test/MockUniswapV3Pool.sol
@@ -0,0 +1,227 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+
+import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
+
+/// @dev Stripped-down essentials of a UniswapV3Pool for oracle tests
+contract MockUniswapV3Pool {
+    address public immutable token0;
+    address public immutable token1;
+
+    // Stripped-down slot0
+    struct Slot0 {
+        int24 tick;
+        uint16 observationIndex;
+        uint16 observationCardinality;
+    }
+    Slot0 public _slot0;
+
+    struct Observation {
+        uint32 blockTimestamp;
+        int56 tickCumulative;
+        bool initialized;
+    }
+    Observation[] public _observations;
+
+    constructor(
+        address _tokenA,
+        address _tokenB,
+        uint16 _observationCardinality
+    ) {
+        PoolAddress.PoolKey memory poolKey = PoolAddress.getPoolKey(
+            _tokenA,
+            _tokenB,
+            uint24(0) // pool fee is unused
+        );
+
+        token0 = poolKey.token0;
+        token1 = poolKey.token1;
+
+        _slot0.observationCardinality = _observationCardinality;
+        for (; _observationCardinality > 0; --_observationCardinality) {
+            _observations.push();
+        }
+    }
+
+    /*****************************
+     * Used by SpotOracleLibrary *
+     *****************************/
+
+    function slot0()
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            uint8 feeProtocol,
+            bool unlocked
+        )
+    {
+        tick = _slot0.tick;
+        observationIndex = _slot0.observationIndex;
+        observationCardinality = _slot0.observationCardinality;
+        // Other parts of slot0 are not used
+    }
+
+    function observations(uint256 _index)
+        external
+        view
+        returns (
+            uint32 blockTimestamp,
+            int56 tickCumulative,
+            uint160 secondsPerLiquidityCumulativeX128,
+            bool initialized
+        )
+    {
+        require(
+            _index < _slot0.observationCardinality,
+            'MockUniswapV3Pool#observations called with invalid index (must be < observationCardinality)'
+        );
+        Observation memory observation = _observations[_index];
+
+        blockTimestamp = observation.blockTimestamp;
+        tickCumulative = observation.tickCumulative;
+        initialized = observation.initialized;
+        // Other parts of observation are not used
+    }
+
+    /*************************
+     * Used by OracleLibrary *
+     *************************/
+
+    function observe(uint32[] calldata _secondsAgos)
+        external
+        view
+        returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s)
+    {
+        require(_secondsAgos.length == 2, 'MockUniswapV3Pool#observe called with invalid array length (must be 2)');
+        int56[] memory _tickCumulatives = new int56[](2);
+        _tickCumulatives[0] = _calculateTickCumulative(_secondsAgos[0]);
+        _tickCumulatives[1] = _calculateTickCumulative(_secondsAgos[1]);
+
+        // secondsPerLiquidityCumulativeX128s is not used
+        uint160[] memory _secondsPerLiquidityCumulativeX128s = new uint160[](2);
+
+        return (_tickCumulatives, _secondsPerLiquidityCumulativeX128s);
+    }
+
+    /**********************
+     * Mocking management *
+     **********************/
+
+    function setObservations(uint32[] calldata _blockTimestamps, int56[] calldata _tickCumulatives) external {
+        require(
+            _blockTimestamps.length == _tickCumulatives.length,
+            'MockUniswapV3Pool#setObservations called with invalid array lengths (must be matching)'
+        );
+        require(
+            _blockTimestamps.length <= _slot0.observationCardinality,
+            'MockUniswapV3Pool#setObservations called with invalid array lengths (must be < slot0.observationCardinality)'
+        );
+
+        for (uint256 ii; ii < _blockTimestamps.length; ++ii) {
+            _observations[ii] = Observation(_blockTimestamps[ii], _tickCumulatives[ii], true);
+        }
+    }
+
+    function setSlot0(int24 _tick, uint16 _observationIndex) external {
+        require(
+            _observationIndex < _slot0.observationCardinality,
+            'MockUniswapV3Pool#setSlot0 called with invalid observationIndex (must be < slot0.observationCardinality)'
+        );
+
+        _slot0.tick = _tick;
+        _slot0.observationIndex = _observationIndex;
+    }
+
+    /*************
+     * Internals *
+     *************/
+
+    /// @dev Mimic Oracle#observeSingle's behaviour
+    ///      See https://github.com/Uniswap/uniswap-v3-core/blob/main/contracts/libraries/Oracle.sol#L245
+    function _calculateTickCumulative(uint32 _secondsAgo) private view returns (int56 tickCumulative) {
+        uint32 currentTimestamp = _blockTimestamp();
+        uint32 target = currentTimestamp - _secondsAgo;
+
+        uint16 currentObsIndex = _slot0.observationIndex;
+        uint16 currentObsCardinality = _slot0.observationCardinality;
+
+        for (uint16 ii; ii < _observations.length; ++ii) {
+            uint16 obsIndex = _prevObservationIndex(ii, currentObsIndex, currentObsCardinality);
+            Observation memory obs = _observations[obsIndex];
+
+            if (_lte(currentTimestamp, obs.blockTimestamp, target)) {
+                if (obs.blockTimestamp == target) {
+                    // Timestamp matches exactly
+                    return obs.tickCumulative;
+                } else if (ii == 0) {
+                    // Last observation matched but needs adjustment to target
+                    // We avoid using slot0 because it's set separately, instead adjusting based on
+                    // historical observations' tick cumulatives
+                    uint16 priorIndex = _prevObservationIndex(1, currentObsIndex, currentObsCardinality);
+                    Observation memory priorObs = _observations[priorIndex];
+
+                    int24 tick = _calculateTickValueFromObservations(obs, priorObs);
+                    uint32 delta = target - obs.blockTimestamp;
+                    return obs.tickCumulative + int56(tick) * delta;
+                } else {
+                    // Older observation matched but needs adjustment to target with next observation (in future)
+                    uint16 nextIndex = _prevObservationIndex(ii - 1, currentObsIndex, currentObsCardinality);
+                    Observation memory nextObs = _observations[nextIndex];
+
+                    int24 nextTick = _calculateTickValueFromObservations(nextObs, obs);
+                    uint32 delta = target - obs.blockTimestamp;
+                    return obs.tickCumulative + int56(nextTick) * delta;
+                }
+            }
+        }
+
+        revert('MockUniswapV3Pool#_calculateTickCumulative could not find matching observation');
+    }
+
+    function _prevObservationIndex(
+        uint16 _prevSteps,
+        uint16 _current,
+        uint16 _cardinality
+    ) private pure returns (uint16) {
+        if (_current < _prevSteps) {
+            return _cardinality - _prevSteps + _current;
+        } else {
+            return _current - _prevSteps;
+        }
+    }
+
+    function _calculateTickValueFromObservations(Observation memory _new, Observation memory _old)
+        private
+        pure
+        returns (int24 tick)
+    {
+        uint32 delta = _new.blockTimestamp - _old.blockTimestamp;
+        return int24((_new.tickCumulative - _old.tickCumulative) / delta);
+    }
+
+    /// @dev Comparator for 32-bit timestamps
+    ///      See https://github.com/Uniswap/uniswap-v3-core/blob/main/contracts/libraries/Oracle.sol#L128
+    function _lte(
+        uint32 _time,
+        uint32 _a,
+        uint32 _b
+    ) private pure returns (bool) {
+        // if there hasn't been overflow, no need to adjust
+        if (_a <= _time && _b <= _time) return _a <= _b;
+
+        uint256 aAdjusted = _a > _time ? _a : _a + 2**32;
+        uint256 bAdjusted = _b > _time ? _b : _b + 2**32;
+
+        return aAdjusted <= bAdjusted;
+    }
+
+    function _blockTimestamp() private view returns (uint32) {
+        // truncation is desired
+        return uint32(block.timestamp);
+    }
+}
---
+++ test/unit/MockUniswapV3Pool.js
@@ -0,0 +1,283 @@
+const hre = require('hardhat')
+const { expect } = require('chai')
+const Timer = require('../Timer')
+const { toBn } = require('../math')
+
+const { ethers } = hre
+
+describe('MockUniswapV3Pool', () => {
+  let tokenA, tokenB
+  let pool
+  const observationCardinality = toBn('3')
+
+  const timer = new Timer(hre)
+
+  beforeEach('setup mocks', async () => {
+    const accounts = await ethers.getSigners()
+    tokenA = accounts[1].address
+    tokenB = accounts[2].address
+
+    const poolFactory = await ethers.getContractFactory('MockUniswapV3Pool')
+    pool = await poolFactory.deploy(tokenA, tokenB, observationCardinality)
+  })
+
+  context('#slot0', () => {
+    it('constructed with correct observation cardinality', async () => {
+      const { observationCardinality: readCardinality } = await pool.slot0()
+      expect(readCardinality).to.equal(observationCardinality)
+    })
+
+    it('rest of slot0 defaults to 0', async () => {
+      const { sqrtPriceX96, tick, observationIndex, observationCardinalityNext, feeProtocol, unlocked } =
+        await pool.slot0()
+
+      expect(sqrtPriceX96).to.equal(0)
+      expect(tick).to.equal(0)
+      expect(observationIndex).to.equal(0)
+      expect(observationCardinalityNext).to.equal(0)
+      expect(feeProtocol).to.equal(0)
+      expect(unlocked).to.be.false
+    })
+
+    context('#setSlot0', () => {
+      it('cannot set invalid observation index', async () => {
+        await expect(pool.setSlot0(0, observationCardinality.add('1'))).to.be.revertedWith(
+          'MockUniswapV3Pool#setSlot0 called with invalid observationIndex (must be < slot0.observationCardinality)'
+        )
+      })
+    })
+
+    context('with slot0 set', () => {
+      const newTick = toBn('5000')
+      const newObservationIndex = toBn('1')
+
+      beforeEach('', async () => {
+        await pool.setSlot0(newTick, newObservationIndex)
+      })
+
+      it('reads slot0', async () => {
+        const {
+          sqrtPriceX96,
+          tick,
+          observationIndex,
+          observationCardinality,
+          observationCardinalityNext,
+          feeProtocol,
+          unlocked,
+        } = await pool.slot0()
+
+        expect(tick).to.equal(newTick)
+        expect(observationIndex).to.equal(newObservationIndex)
+        expect(observationCardinality).to.equal(observationCardinality)
+
+        // Rest of slot0 is untouched
+        expect(sqrtPriceX96).to.equal(0)
+        expect(observationCardinalityNext).to.equal(0)
+        expect(feeProtocol).to.equal(0)
+        expect(unlocked).to.be.false
+      })
+    })
+  })
+
+  context('#observations', () => {
+    const observations = [
+      [toBn('10000000'), toBn('5000')],
+      [toBn('10000001'), toBn('5500')],
+    ]
+    const observationTimes = observations.map(([time]) => time)
+    const observationTicks = observations.map(([_, cumulativeTick]) => cumulativeTick)
+
+    context('#setObservations', () => {
+      it('can set observations with matching arrays', async () => {
+        await pool.setObservations(observationTimes, observationTicks)
+      })
+
+      it('cannot set observations with non-matching arrays', async () => {
+        await expect(pool.setObservations(observationTimes, [observationTicks[0]])).to.be.revertedWith(
+          'MockUniswapV3Pool#setObservations called with invalid array lengths (must be matching)'
+        )
+      })
+
+      it('cannot set observations larger than cardinality', async () => {
+        const tooLargeTimes = Array(observationCardinality.toNumber() + 1).fill(toBn('1000000'))
+        const tooLargeTicks = Array(observationCardinality.toNumber() + 1).fill(toBn('5000'))
+        await expect(pool.setObservations(tooLargeTimes, tooLargeTicks)).to.be.revertedWith(
+          'MockUniswapV3Pool#setObservations called with invalid array lengths (must be < slot0.observationCardinality)'
+        )
+      })
+    })
+
+    context('without observations', () => {
+      it('all cardinality is left uninitialized', async () => {
+        for (index = 0; index < observationCardinality; ++index) {
+          const observation = await pool.observations(index)
+          expect(observation.initialized).to.be.false
+        }
+      })
+    })
+
+    context('with observations', () => {
+      beforeEach('set observations', async () => {
+        await pool.setObservations(observationTimes, observationTicks)
+      })
+
+      it('reads set observations', async () => {
+        const observation1 = await pool.observations(0)
+        expect(observation1.blockTimestamp).to.equal(observationTimes[0])
+        expect(observation1.tickCumulative).to.equal(observationTicks[0])
+        expect(observation1.initialized).to.be.true
+
+        const observation2 = await pool.observations(1)
+        expect(observation2.blockTimestamp).to.equal(observationTimes[1])
+        expect(observation2.tickCumulative).to.equal(observationTicks[1])
+        expect(observation2.initialized).to.be.true
+      })
+
+      it('remaining cardinality is left uninitialized', async () => {
+        const observation3 = await pool.observations(2)
+        expect(observation3.blockTimestamp).to.equal(0)
+        expect(observation3.tickCumulative).to.equal(0)
+        expect(observation3.initialized).to.be.false
+      })
+
+      it('cannot read observations outside cardinality', async () => {
+        await expect(pool.observations(3)).to.be.revertedWith(
+          'MockUniswapV3Pool#observations called with invalid index (must be < observationCardinality)'
+        )
+      })
+    })
+  })
+
+  context('#observe', () => {
+    const observationIndex = 1
+    const observations = [
+      [toBn('100'), toBn('5000')], // prior observation
+      [toBn('200'), toBn('6000')], // current observation
+      [toBn('-100'), toBn('4500')], // wrapped cardinality
+    ]
+    const observationTimeDeltas = observations.map(([time]) => time)
+    const observationTicks = observations.map(([_, cumulativeTick]) => cumulativeTick)
+    const emptySecondsPerLiquidityCumulative = [toBn('0'), toBn('0')]
+
+    async function setupMocks(chainTimeIncrease) {
+      const now = await timer.now()
+      const observationTimes = observationTimeDeltas.map((delta) => now.add(delta))
+      await pool.setObservations(observationTimes, observationTicks)
+      await timer.setTime(now.add(chainTimeIncrease))
+    }
+
+    beforeEach('set observation index', async () => {
+      await pool.setSlot0(0, observationIndex)
+    })
+
+    it('cannot observe with invalid seconds ago (must be 2)', async () => {
+      const oneMinute = toBn('60')
+      await expect(pool.observe([oneMinute])).to.be.revertedWith(
+        'MockUniswapV3Pool#observe called with invalid array length (must be 2)'
+      )
+      await expect(pool.observe([oneMinute, oneMinute, oneMinute])).to.be.revertedWith(
+        'MockUniswapV3Pool#observe called with invalid array length (must be 2)'
+      )
+    })
+
+    context('with exactly matching times', () => {
+      beforeEach('setup mocks', async () => {
+        chainTimeIncrease = toBn('200') // match current observation
+        await setupMocks(chainTimeIncrease)
+      })
+
+      it('observes correctly', async () => {
+        const [tickCumulatives, secondsPerLiquidityCumulatives] = await pool.observe([
+          0, // match current observation
+          100, // match prior observation
+        ])
+        expect(tickCumulatives).to.deep.equal([observationTicks[1], observationTicks[0]])
+        expect(secondsPerLiquidityCumulatives).to.deep.equal(emptySecondsPerLiquidityCumulative)
+      })
+    })
+
+    context('with interpolated end time', () => {
+      beforeEach('setup mocks', async () => {
+        chainTimeIncrease = toBn('300') // extend past current observation
+        await setupMocks(chainTimeIncrease)
+      })
+
+      it('observes correctly', async () => {
+        const [tickCumulatives, secondsPerLiquidityCumulatives] = await pool.observe([
+          0, // extrapolate from current observation to now
+          200, // match prior observation (300 - 200 = 100)
+        ])
+
+        // current observation's cumulative tick = 6000
+        // selected time - current observation time = 100s
+        // tick delta / s between current and last = 10 tick / s
+        // 6000 + 100 * 10 => 7000
+        const interpolatedEndTickCumulative = toBn('7000')
+        expect(tickCumulatives).to.deep.equal([interpolatedEndTickCumulative, observationTicks[0]])
+        expect(secondsPerLiquidityCumulatives).to.deep.equal(emptySecondsPerLiquidityCumulative)
+      })
+    })
+
+    context('with interpolated start time', () => {
+      beforeEach('setup mocks', async () => {
+        chainTimeIncrease = toBn('200') // match current observation
+        await setupMocks(chainTimeIncrease)
+      })
+
+      it('observes correctly', async () => {
+        const [tickCumulatives, secondsPerLiquidityCumulatives] = await pool.observe([
+          0, // match current observation
+          50, // extrapolate from current and prior observation (200 - 50 = 150)
+        ])
+
+        // prior observation's cumulative tick = 5000
+        // target - prior's observation time = 50s
+        // tick delta / s between current and prior = 10 tick / s
+        // 5000 + 50 * 10 => 5500
+        const interpolatedStartTickCumulative = toBn('5500')
+        expect(tickCumulatives).to.deep.equal([observationTicks[1], interpolatedStartTickCumulative])
+        expect(secondsPerLiquidityCumulatives).to.deep.equal(emptySecondsPerLiquidityCumulative)
+      })
+    })
+
+    context('with wrapping cardinality', () => {
+      beforeEach('setup mocks', async () => {
+        chainTimeIncrease = toBn('200') // match current observation
+        await setupMocks(chainTimeIncrease)
+      })
+
+      it('observes correctly', async () => {
+        const [tickCumulatives, secondsPerLiquidityCumulatives] = await pool.observe([
+          0, // match current observation
+          300, // match prior-1 observation (200 - 300 = -100)
+        ])
+
+        expect(tickCumulatives).to.deep.equal([observationTicks[1], observationTicks[2]])
+        expect(secondsPerLiquidityCumulatives).to.deep.equal(emptySecondsPerLiquidityCumulative)
+      })
+    })
+
+    context('with rounded interpolated time', () => {
+      beforeEach('setup mocks', async () => {
+        chainTimeIncrease = toBn('200') // match current observation
+        await setupMocks(chainTimeIncrease)
+      })
+
+      it('observes correctly', async () => {
+        const [tickCumulatives, secondsPerLiquidityCumulatives] = await pool.observe([
+          0, // match current observation
+          150, // extrapolate from prior and prior-1 observation (200 - 150 = 50)
+        ])
+
+        // prior-1 observation's cumulative tick = 4500
+        // target - prior-1's observation time = 150s
+        // tick delta / s between prior and prior-1 = 2.5 tick / s
+        // tick delta / s floored to whole number = 2 tick / s
+        // 4500 + 150 * 2 => 4800
+        const interpolatedStartTickCumulative = toBn('4800')
+        expect(tickCumulatives).to.deep.equal([observationTicks[1], interpolatedStartTickCumulative])
+        expect(secondsPerLiquidityCumulatives).to.deep.equal(emptySecondsPerLiquidityCumulative)
+      })
+    })
+  })
+})
